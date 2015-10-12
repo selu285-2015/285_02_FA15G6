4be1cb3ce47044c65b8d813118fe2db67b4a44bc
Aaron Dentro <Aaron.Dentro@selu.edu>
2015-10-11 23:56:58 -0500

    Final Mid-term commit.

---
+++ 285ProjectDemo/Controllers/UsersController.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using _285ProjectDemo;
+using _285ProjectDemo.Models;
+using System.Web.Helpers;
+
+namespace _285ProjectDemo.Controllers
+{
+    public class UsersController : Controller
+    {
+        private DataContext _db = new DataContext();
+
+        // GET: Users
+        public ActionResult Index()
+        {
+            return View(_db.Users.ToList());
+        }
+
+        // GET: Users/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            User user = _db.Users.Find(id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            return View(user);
+        }
+
+        // GET: Users/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Users/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,FirstName,LastName,Major,Username,Password")] User createUser)
+        {
+            if (ModelState.IsValid)
+            {
+                createUser.Salt = Crypto.GenerateSalt();
+                createUser.Password += createUser.Salt;
+                createUser.Password = Crypto.HashPassword(createUser.Password);
+                _db.Users.Add(createUser);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            
+            return View(createUser);
+        }
+
+        [HttpPost]
+        public ActionResult Login(string username, string password)
+        {
+            User validate = _db.Users.FirstOrDefault(dbUsers => dbUsers.Username == username);
+            if (validate != null)
+            {
+                if (Crypto.VerifyHashedPassword(validate.Password, password + validate.Salt))
+                {
+                    //DO Stuff!!! WE LOGGED IN!!!
+                    
+                }
+            }
+            return RedirectToAction("Create");
+        }
+
+        // GET: Users/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            User user = _db.Users.Find(id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            return View(user);
+        }
+
+        // POST: Users/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,FirstName,LastName,Major,Username,Password")] User user)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Entry(user).State = EntityState.Modified;
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(user);
+        }
+
+        // GET: Users/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            User user = _db.Users.Find(id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            return View(user);
+        }
+
+        // POST: Users/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            User user = _db.Users.Find(id);
+            _db.Users.Remove(user);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                _db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ 285ProjectDemo/ViewModels/Questionaire.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using _285ProjectDemo.Migrations;
+using _285ProjectDemo.Models;
+
+
+namespace _285ProjectDemo.ViewModels
+{
+    public class Questionaire
+    {
+        public List<QuestionModel> Questions {get; set;}
+        public string category { get; set; }
+    }
+}
