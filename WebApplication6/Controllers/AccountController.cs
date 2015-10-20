92eb62371bd6bdace1bcf1fd6e3f681fd80e145b
ScottTanner <scott.tanner@selu.edu>
2015-10-20 17:18:55 -0500

    Project from Mid-Term

--- WebApplication6/Controllers/AccountController.cs
+++ WebApplication6/Controllers/AccountController.cs
@@ -6,16 +6,18 @@ using System.Web;
 using System.Web.Mvc;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.SqlClient;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using WebApplication6.Models;
 
 namespace WebApplication6.Controllers
 {
     [AllowAnonymous]
     public class AccountController : Controller
     {
-
-        public AccountController()
-        {
-        }
 
         public ActionResult Register()
         {
@@ -25,6 +27,39 @@ namespace WebApplication6.Controllers
         public ActionResult Login()
         {
             return View();
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Register(User U)
+        {
+            if (ModelState.IsValid)
+            {
+                using (DataBaseEntities db = new DataBaseEntities())
+                {
+
+                    db.Users.Add(U);    
+                    db.SaveChanges();
+                    ModelState.Clear();
+                    U = null;
+                    ViewBag.Message = "Registration Done";
+                }
+            }
+            return RedirectToAction("userPage" , "Userpage");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(User U)
+        {
+            if (ModelState.IsValid)
+            {
+                
+
+
+            }
+            return View(U);
         }
 
     }
---
+++ WebApplication6/Models/UserViewModel.cs
@@ -0,0 +1,38 @@
+namespace WebApplication6.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("User")]
+    public partial class UserViewModel
+    {
+        public int UserID { get; set; }
+
+        [Required(ErrorMessage = "Provide a Username", AllowEmptyStrings = false)]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Provide a Password", AllowEmptyStrings = false)]
+        [DataType(System.ComponentModel.DataAnnotations.DataType.Password)]
+        [StringLength(50, MinimumLength = 8, ErrorMessage = "Password must be at least 8 character long.")]
+        public string Password { get; set; }
+
+        [System.ComponentModel.DataAnnotations.Compare("Password", ErrorMessage = "Confirm Password does not match Password.")]
+        public string ConfirmPassword { get; set; }
+
+        [Required(ErrorMessage = "Please Provide First Name")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Please Provide Last Name")]
+        public string LastName { get; set; }
+
+        [RegularExpression(@"^([0-9a-zA-Z]([\+\-_\.][0-9a-zA-Z]+)*)+@(selu.edu)$",
+                ErrorMessage = "Please provide valid email id")]
+        public string EmailID { get; set; }
+
+
+
+    }
+}
