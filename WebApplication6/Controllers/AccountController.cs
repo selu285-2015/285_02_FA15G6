f224b6c6885830c1ee30028250521ae4fda4cbf7
ScottTanner <scott.tanner@selu.edu>
2015-10-29 14:55:12 -0500

    Email Confirm

--- WebApplication6/Controllers/AccountController.cs
+++ WebApplication6/Controllers/AccountController.cs
@@ -12,12 +12,16 @@ using System.Data.SqlClient;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 using WebApplication6.Models;
+using System.Web.Helpers;
+using System.Net.Mail;
+using System.Net;
 
 namespace WebApplication6.Controllers
 {
     [AllowAnonymous]
     public class AccountController : Controller
     {
+        DataBaseEntities db = new DataBaseEntities();
 
         public ActionResult Register()
         {
@@ -29,6 +33,17 @@ namespace WebApplication6.Controllers
             return View();
         }
 
+        public ActionResult confirmEmail()
+        {
+
+            return View();
+        }
+
+        public ActionResult confirm(object sender , EventArgs e )
+        {
+            //U.active = true;
+            return RedirectToAction("userPage", "Userpage");
+        }
 
         [HttpPost]
         [ValidateAntiForgeryToken]
@@ -38,28 +53,57 @@ namespace WebApplication6.Controllers
             {
                 using (DataBaseEntities db = new DataBaseEntities())
                 {
+                    //u.salt = crypto.generatesalt();
+                    //u.password += u.salt;
+                    //u.password = crypto.hashpassword(u.password);
 
-                    db.Users.Add(U);    
-                    db.SaveChanges();
-                    ModelState.Clear();
+                    //db.users.add(u);
+                    //db.savechanges();
+                    //modelstate.clear();
+                    //
+                    MailMessage m = new MailMessage();
+                    SmtpClient sc = new SmtpClient();
+                    try
+                    {
+                        m.From = new MailAddress("selugroup6.maneattraction@gmail.com");
+                        m.To.Add(U.EmailID);
+                        m.Subject = "Confirm";
+                        m.IsBodyHtml = true;
+                        m.Body = "Click http://localhost:53460/Userpage/userPage to finish registration!";
+                        sc.Host = "smtp.gmail.com";
+                        sc.Port = 587;
+                        sc.Credentials = new System.Net.NetworkCredential("selugroup6.maneattraction@gmail.com", "burrisislove");
+
+                        sc.EnableSsl = true;
+                        sc.Send(m);
+                        Response.Write("Email Send successfully");
+                    }
+                    catch (Exception ex)
+                    {
+                        Response.Write(ex.Message);
+                    }
+                    
+                    //
+
                     U = null;
                     ViewBag.Message = "Registration Done";
                 }
             }
-            return RedirectToAction("userPage" , "Userpage");
+            return RedirectToAction("Home" , "Index");
         }
 
+
         [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Login(User U)
+        public ActionResult Login(string username, string password)
         {
-            if (ModelState.IsValid)
+            User validate = db.Users.FirstOrDefault(dbUsers => dbUsers.UserName == username);
+            if (validate != null)
             {
-                
-
-
+  //              if (Crypto.VerifyHashedPassword(validate.Password, password + validate.Salt))
+                {
+                }
             }
-            return View(U);
+            return RedirectToAction("userPage");
         }
 
     }
